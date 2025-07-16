2cacbbba7bf6addc1148981c4d71cf0615fff3d8
hameed <mohamedishaq499@gmail.com>
2025-07-16 22:49:23 +0530

    task

--- Day11/ems/src/components/Footer.jsx
+++ Day11/ems/src/components/Footer.jsx
@@ -12,7 +12,11 @@ const Footer = () => {
       borderTop: '2px solid #4a4e69',
       boxShadow: '0 -2px 8px rgba(74, 78, 105, 0.10)',
       fontFamily: 'Segoe UI, Arial, sans-serif',
-      marginTop: '2rem',}
+      marginTop: '2rem',
+      display:'flex',
+      alignItems:'',
+
+    }
   };
 
   return (
